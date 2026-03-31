03c00e1927d7496b0dd5f45d24b26d2ff82a9b41
Maroua <naitslimanimaroua@gmail.com>
2026-03-31 18:16:25 +0200

    correcting a request

--- src/queries/queries_Adele/queriesAdele.sql
+++ src/queries/queries_Adele/queriesAdele.sql
@@ -1,11 +1,10 @@
 -- Comparaison FR vs UK : nb accidents par sexe et par tranche de gravité 
-
-SELECT p.nom_pays, u.sexe, f.indice_gravite, COUNT(DISTINCT f.id_accident) AS nb_accidents
+SELECT p.nom_pays, u.sexe, f.indice_gravite
 FROM FAIT_ACCIDENT f 
 JOIN DIM_PAYS p ON p.id_pays = f.id_pays
 JOIN DIM_USAGER u ON u.id_usager = f.id_usager 
 GROUP BY ROLLUP(p.nom_pays, u.sexe, f.indice_gravite)
-ORDER BY p.nom_pays NULLS LAST, f.indice_gravite NULLS LAST 
+ORDER BY p.nom_pays NULLS LAST, f.indice_gravite DESC NULLS LAST
 LIMIT 10;
 
 -- Les accidents sont-ils plus graves pendant les jours feriés selon la météo
